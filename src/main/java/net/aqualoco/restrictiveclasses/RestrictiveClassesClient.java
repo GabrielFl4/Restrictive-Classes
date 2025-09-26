0aee877ab34b03aa17b72b60a4d2c3f9cf78ea7a
AquaLoco <gabrielmorandim05@gmail.com>
2025-09-26 15:41:49 -0300

    Initial commit

---
+++ src/main/java/net/aqualoco/restrictiveclasses/RestrictiveClassesClient.java
@@ -0,0 +1,10 @@
+package net.aqualoco.restrictiveclasses;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RestrictiveClassesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/aqualoco/restrictiveclasses/RestrictiveClassesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.aqualoco.restrictiveclasses;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RestrictiveClassesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
